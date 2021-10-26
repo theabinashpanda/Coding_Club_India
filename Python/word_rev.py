2e92a01e5c33a9c501eee1ca8dac97ffd5463e93
Abinash Panda <77439575+theabinashpanda@users.noreply.github.com>
2021-10-26 12:46:26 +0530

    Create word_rev.py

---
+++ Python/word_rev.py
@@ -0,0 +1,8 @@
+'''
+Reverse words in a given String in Python
+Date: 26/10/2021
+Time: 08:01:23
+'''
+sentence=input("Enter a sentence: ")
+words=sentence.split(" ")
+print(" ".join(reversed(words)))
